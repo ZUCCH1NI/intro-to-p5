3c340c3da9b320e224da5a1d1389b6138d9dcf1a
Zania Rogers <zrogers@wlapcs.org>
2017-03-02 01:09:15 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup(){
+    createCanvas(600,400);
+    background(139, 255, 104);
+}
+
+function draw(){
+    line(35, 80, 400, 200);
+    
+    stroke(255,255,255);
+    fill(247, 40, 255);
+    rect(290,100,250,250);
+    
+    rect(145,275,450,100);
+   
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(99, 249, 234);
+    ellipse(50,250,80,80);
+    
+    ellipse(525,35,150,50);
+    
+        line(550,90,200,350);
+    
+    line(100,345,100,10);
+    
+    line(300,300,575,300);
+} 
+//draw me 2 ellipses
+//draw me 2 rectangles
+//draw me 4 lines(1 with a positive slope,1 with a neg slope)
+//1 with a slope of 0, and 1 with a slope of undefined.
